3fc083b5e3acda6dc15311242d5dc462e3904aa6
flannel jesus <sjoshuaross@yahoo.com>
2016-09-11 23:40:54 +0100

    refactored bs4 parsing pages

--- main.py
+++ main.py
@@ -2,28 +2,28 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-home = requests.get("http://hltv.org/").text
-home = BeautifulSoup(home, "lxml")
 
-matches = requests.get("http://www.hltv.org/matches/").text
-matches = BeautifulSoup(matches, "lxml")
+def get_parsed_page(url):
+    return BeautifulSoup(requests.get(url).text, "lxml")
 
 
 def top5teams():
+    home = get_parsed_page("http://hltv.org/")
     count = 0
     teams = []
-    for team in home.find_all("div", {"class": "vsbox",})[:5]:
+    for team in home.find_all("div", {"class": "vsbox", })[:5]:
         count += 1
         teamname = team.find_all("div")[2].text.strip()
         teams.append(teamname)
     return teams
 
+
 def getmatches():
-    match_data = []
+    matches = get_parsed_page("http://www.hltv.org/matches/")
     matchlist = matches.find_all("div", {"class": ["matchListBox", "matchListDateBox"]})
     for match in matchlist:
         if match['class'][0] == "matchListDateBox":
-            print("* " +  match.text)
+            print("* " + match.text)
         else:
             try:
                 time = match.find("div", {"class": "matchTimeCell"}).text.strip()
@@ -32,6 +32,7 @@ def getmatches():
                 print(time + " " + team1 + " vs " + team2)
             except:
                 print(match.text[:7].strip(), match.text[7:-7].strip())
-    
+
+
 if __name__ == "__main__":
     getmatches()
