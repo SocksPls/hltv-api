a87b1ef06858d6d9888c531a035b7fb28fb4931c
flannel jesus <sjoshuaross@yahoo.com>
2016-09-11 23:42:36 +0100

    top20teams function
    I'm using dictionaries because dictionaries are basically JSON already.
    We can talk about this decision if you don't like it. I'm just assuming
    we'll be exporting JSON at the end of this all.

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def get_parsed_page(url):
     return BeautifulSoup(requests.get(url).text, "lxml")
 
 
+def parseint(string):
+    return int(''.join([x for x in string if x.isdigit()]))
+
+
 def top5teams():
     home = get_parsed_page("http://hltv.org/")
     count = 0
@@ -16,6 +20,27 @@ def top5teams():
         teamname = team.find_all("div")[2].text.strip()
         teams.append(teamname)
     return teams
+
+
+def top20teams():
+    page = get_parsed_page("http://www.hltv.org/ranking/teams/")
+    teams = page.select("div.ranking-box")
+    teamlist = []
+    for team in teams:
+        newteam = {'name': team.select('.ranking-teamName > a')[0].text.strip(),
+                   'rank': parseint(team.select('.ranking-number')[0].text.strip()),
+                   'rank-points': parseint(team.select('.ranking-teamName > span')[0].text),
+                   'team-id': parseint(team.select('.ranking-delta')[0].get('id')),
+                   'team-players': []}
+        for player_div in team.select('.ranking-lineup > div'):
+            player = {}
+            player_anchor = player_div.select('.ranking-playerNick > a')[0]
+            player['name'] = player_anchor.text.strip()
+            player['player-id'] = parseint(player_anchor.get('href'))
+            newteam['team-players'].append(player)
+        teamlist.append(newteam)
+    return teamlist
+
 
 
 def getmatches():
@@ -33,6 +58,5 @@ def getmatches():
             except:
                 print(match.text[:7].strip(), match.text[7:-7].strip())
 
-
 if __name__ == "__main__":
-    getmatches()
+    print(top20teams())
