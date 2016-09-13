30404fbd8f41f6ef697750dcf9fcaa472e850a3a
SecretlyTaco <secretlytaco@gmail.com>
2016-09-13 01:30:17 +0100

    Removed redundant code

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def top_players():
         for player_elem in box.select("> div"):
             player = {}
             player_link = player_elem.find('a')
-            player['name'] = player_link.text.replace(")", ") ")
+            player['name'] = player_link.text
             p_url = player_link['href']
             player['player-id'] = converters.to_int(p_url[p_url.index('playerid=')+9:p_url.index('&statsfilter')])
             player['stat'] = player_elem.select('div:nth-of-type(2)')[0].text
